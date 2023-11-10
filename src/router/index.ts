ac1c6fe39342a5cb707637e214ed24b5e8f23f61
arthurtlima <arthur.alves.lima@live.com>
2023-11-10 00:37:45 -0300

    feat: creating authentication rounting and loggin in

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import authentications from './authentications';
+
+const router = express.Router();
+
+export default (): express.Router => {
+    authentications(router);
+    
+    return router;
+}
