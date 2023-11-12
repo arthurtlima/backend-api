c0d741e2599af910fbf851a39887cf2388df2906
arthurtlima <arthur.alves.lima@live.com>
2023-11-12 00:47:12 -0300

    feat: Add Users controller and routing
    Signed-off-by: arthurtlima <arthur.alves.lima@live.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import authentications from './authentications';
+import users from './users';
 
 const router = express.Router();
 
 export default (): express.Router => {
     authentications(router);
+    users(router);
     
     return router;
 }
