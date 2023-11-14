5a1ae4a739dbb108e1359d8afe527474f6d92dcb
arthurtlima <arthur.alves.lima@live.com>
2023-11-14 18:05:12 -0300

    refactor: db

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,4 +1,14 @@
 import mongoose from 'mongoose'
+interface CreateUser {
+  email: string
+  username: string
+  authentication: {
+    salt: string
+    password: string
+  }
+}
+
+type UpdateUser = Pick<CreateUser, 'username'>
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -13,25 +23,21 @@ const UserSchema = new mongoose.Schema({
 export const UserModel = mongoose.model('User', UserSchema)
 
 export const getUsers = () => UserModel.find()
+
 export const getUserByEmail = (email: string) => UserModel.findOne({ email })
+
 export const getUserBySessionToken = (sessionToken: string) =>
   UserModel.findOne({ 'authentication.sessionToken': sessionToken })
+
 export const getUserById = (id: string) => UserModel.findById(id)
 
-interface CreateUserValues {
-  email: string
-  username: string
-  authentication: {
-    salt: string
-    password: string
-  }
+export const createUser = async (values: CreateUser) => {
+  const user = await new UserModel(values).save()
+  return user.toObject()
 }
 
-type UpdateUserUsername = Pick<CreateUserValues, 'username'>
-
-export const createUser = (values: CreateUserValues) =>
-  new UserModel(values).save().then((user) => user.toObject())
 export const deleteUserById = (id: string) =>
   UserModel.findOneAndDelete({ _id: id })
-export const updateUserById = (id: string, values: UpdateUserUsername) =>
+
+export const updateUserById = (id: string, values: UpdateUser) =>
   UserModel.findByIdAndUpdate(id, values)
