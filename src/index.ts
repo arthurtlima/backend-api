de9cc3d0590cea0e360c4a5fe92b277f89185a65
arthurtlima <arthur.alves.lima@live.com>
2023-11-14 16:51:34 -0300

    feat: Add eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,40 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotEnv from 'dotenv';
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotEnv from 'dotenv'
 
-import router from './router';
+import router from './router'
 
-dotEnv.config();
+dotEnv.config()
 
-const app = express();
+const app = express()
 
-app.use(cors({
-    credentials: true
-}));
+app.use(
+  cors({
+    credentials: true,
+  }),
+)
 
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 const port = process.env.PORT || 8080
 
-server.listen(port, ()=> {
-    console.log(`Server running on http://localhost:${port}/`)
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`)
 })
 
 const mongoURL = process.env.MONGO_URL
 
-mongoose.Promise = Promise;
+mongoose.Promise = Promise
 mongoose.connect(mongoURL)
 mongoose.connection.on('error', (error: Error) => console.log(error))
 
-app.use('/', router());
+app.use('/', router())
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from 'express'
 
-import authentications from './authentications';
-import users from './users';
+import authentications from './authentications'
+import users from './users'
 
-const router = express.Router();
+const router = express.Router()
 
 export default (): express.Router => {
-    authentications(router);
-    users(router);
-    
-    return router;
+  authentications(router)
+  users(router)
+
+  return router
 }
