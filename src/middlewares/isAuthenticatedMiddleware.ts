44d64eba92ecaa9eba15ae0956b7eb6444325327
arthurtlima <arthur.alves.lima@live.com>
2023-11-14 22:32:02 -0300

    refactor: middlewares

---
+++ src/middlewares/isAuthenticatedMiddleware.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { merge } from 'lodash'
+import { getUserBySessionToken } from '../services/userService'
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    const sessionToken = req.cookies['USER-AUTH']
+
+    if (!sessionToken) {
+      return res.sendStatus(403)
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken)
+
+    if (!existingUser) {
+      return res.sendStatus(403)
+    }
+
+    merge(req, { identity: existingUser })
+
+    return next()
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(400)
+  }
+}
